cede119ab3af935fb97af5b23971ede9a3e18162
Seughton <37404801+Seughton@users.noreply.github.com>
2018-08-15 23:30:37 +0300

    UPD
    Signed-off-by: Anton <Seughton@gmail.com>

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,4 @@ import popper from 'popper.js';
 import 'slick-carousel'
 import bootstrap from 'bootstrap';
 
-/*$('.slider_item').css('background','red');*/
-let rand = Math.floor(Math.random() * 10);
-$('.Slider').slick({
-   /* autoplay: true,
-    autoplaySpeed: 3000,*/
-   initialSlide: rand,
-   arrows: true,
-
-});
-jQuery(function() {
-    jQuery('body').css('color', 'red');
-});
+/*app*/
